ce06559f164dbfe056f53361872f0e022fc12f44
DJ <mail@chenquanyi.cn>
2022-12-08 15:56:47 +0800

    响应类

---
+++ src/main/java/com/fasturl/shorturlservice/model/Result.java
@@ -0,0 +1,38 @@
+package com.fasturl.shorturlservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 响应类
+ *
+ * @since 2022-12-8 15:34:49
+ * @author quanyi
+ */
+@Data
+@AllArgsConstructor
+public class Result {
+    private int code;
+    private String message;
+    private Object data;
+
+    /**
+     * 成功响应
+     *
+     * @param data 数据
+     * @return 返回Result实例
+     */
+    public static Result success(Object data){
+        return new Result(200,"success",data);
+    }
+
+    /**
+     * 失败响应
+     *
+     * @param message 提示信息
+     * @return 返回Result实例
+     */
+    public static Result fail(String message){
+        return new Result(299,message,null);
+    }
+}
