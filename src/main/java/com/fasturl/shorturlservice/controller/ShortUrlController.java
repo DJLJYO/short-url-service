370ca11d3416370206a081f06caae12cfebc4608
DJ <mail@chenquanyi.cn>
2022-12-10 21:51:32 +0800

    接口调整

--- src/main/java/com/fasturl/shorturlservice/controller/ShortUrlController.java
+++ src/main/java/com/fasturl/shorturlservice/controller/ShortUrlController.java
@@ -3,6 +3,7 @@ package com.fasturl.shorturlservice.controller;
 import com.fasturl.shorturlservice.model.Result;
 import com.fasturl.shorturlservice.model.ShortDefinitional;
 import com.fasturl.shorturlservice.service.ShortDefinitionalService;
+import com.fasturl.shorturlservice.utils.EncodeShortUrl;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -24,12 +25,13 @@ public class ShortUrlController {
      * 重定向跳转
      *
      * @param key      重定向的URL Key
-     * @param response 响应
+     * @param response 响应头
+     * @return Result
      */
     @GetMapping("/{key}")
     public Result sendRedirect(@PathVariable String key, HttpServletResponse response) {
         ShortDefinitional shortDefinitional = shortDefinitionalService.queryOriginUrl(key);
-        // 数据库找不到记录
+        // 数据库有记录
         if (shortDefinitional != null) {
             String redirectUrl = shortDefinitional.getOriginUrl();
             // 301重定向
@@ -43,11 +45,14 @@ public class ShortUrlController {
     /**
      * 缩短网址
      *
-     * @param url 要缩短的网址
+     * @param url URL编码后的网址
      * @return Result
      */
     @RequestMapping("/shorten")
     public Result shortenUrl(String url) {
+        if (!EncodeShortUrl.isUrl(url)) {
+            return Result.fail("不是有效的URL！");
+        }
         return shortDefinitionalService.addUrl(url);
     }
 }
