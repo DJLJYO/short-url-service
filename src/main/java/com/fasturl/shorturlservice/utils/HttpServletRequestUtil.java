38e86d6e8dd89528d9d46e0206ec9ad4d34a5372
DJ <mail@chenquanyi.cn>
2023-05-15 01:25:43 +0800

    HttpServletRequest工具类

---
+++ src/main/java/com/fasturl/shorturlservice/utils/HttpServletRequestUtil.java
@@ -0,0 +1,34 @@
+package com.fasturl.shorturlservice.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HttpServletRequestUtil {
+    /**
+     * 返回服务器加端口号  服务器:端口号
+     *
+     * @param request    HttpServletRequest
+     * @param notDefault 去80端口,端口为80的不加
+     * @return
+     */
+    public static String getServerNamePort(HttpServletRequest request, Boolean notDefault) {
+        // 不带80端口的
+        if (notDefault) {
+            return request.getServerName();
+        }
+        return request.getServerName() + ":" + request.getServerPort();
+    }
+
+    /**
+     * 返回服务器加端口号  服务器:端口号  默认80端口不带
+     *
+     * @param request HttpServletRequest
+     * @return
+     */
+    public static String getServerNamePort(HttpServletRequest request) {
+        // 不带80端口的
+        if (request.getServerPort() == 80) {
+            return getServerNamePort(request, true);
+        }
+        return getServerNamePort(request, false);
+    }
+}
